b0595b4e706eb99dbce611badc132adea074e45b
Aishik Pal <aishikpal.sp@gmail.com>
2023-06-16 14:52:05 +0530

    Post evaluation changes updated

--- ExpenseTrackerApplication/Controllers/TransactionsController.cs
+++ ExpenseTrackerApplication/Controllers/TransactionsController.cs
@@ -17,137 +17,96 @@ namespace ExpenseTrackerApplication.Controllers
     {
         ApplicationDbContext _appdb = new ApplicationDbContext();
 
-        [HttpGet("UserTransactions")]
+        [HttpGet("MyTransactions")]
         [Authorize]
         //https://localhost:7145/api/transactions/usertransactions
         public IActionResult GetTransactionsByUser()
         {
             var userEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
             var user = _appdb.Users.FirstOrDefault(u => u.Email == userEmail);
-            var transaction = _appdb.Transactions.Where(c => c.UserId == user.Id).Select(
-                    t => new TransactionDTO
-                    {
-                        //TransactionId = t.Id,
-                        CategoryName = t.Category.Title,
-                        CategoryType = t.Category.Type.ToLower(),
-                        Amount = t.Amount,
-                        Date = t.Date.ToString("D"),
-                        //ToString("dd-MMM-yyyy"),
-                        Note = t.Note
-                    });
-            if (transaction == null)
+
+            var transactions = _appdb.Transactions.Where(c => c.UserId == user.Id).Select(
+                    t => new TransactionDTO(t.Category.Title, t.Category.Type.ToLower(), t.Amount, t.Note, t.Date.ToString("D")));
+
+            if (transactions == null)
                 return BadRequest();
             else
-                return Ok(transaction);
+                return Ok(transactions);
         }
 
-        [HttpGet("SortedTransactions")]
+        [HttpGet("SortByDate")]
         [Authorize]
         //https://localhost:7145/api/transactions/sortedtransactions
         public IActionResult SortTransactionsByDate()
         {
             var userEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
             var user = _appdb.Users.FirstOrDefault(u => u.Email == userEmail);
-            var transaction = _appdb.Transactions.Where(c => c.UserId == user.Id).OrderBy(x => x.Date);
-            var sortedTransaction = transaction.Select(
-                    t => new TransactionDTO
-                    {
-                        //TransactionId = t.Id,
-                        CategoryName = t.Category.Title,
-                        CategoryType = t.Category.Type.ToLower(),
-                        Amount = t.Amount,
-                        Date = t.Date.ToString("D"),
-                        //ToString("dd-MMM-yyyy"),
-                        Note = t.Note
-                    });
-            if (sortedTransaction == null)
+
+            var transactions = _appdb.Transactions.Where(c => c.UserId == user.Id).OrderBy(x => x.Date).Select(
+                    t => new TransactionDTO(t.Category.Title, t.Category.Type.ToLower(), t.Amount, t.Note, t.Date.ToString("D")));
+
+            if (transactions == null)
                 return BadRequest();
             else
-                return Ok(sortedTransaction);
+                return Ok(transactions);
         }
 
-        [HttpGet("UserTransactionsByCategory")]
+        [HttpGet("CategoryId/{id}")]
         [Authorize]
         //https://localhost:7145/api/transactions/usertransactionsbycategory?categoryid=0
-        public IActionResult GetTransactionsByCategory(int categoryId)
+        public IActionResult GetTransactionsByCategory(string id)
         {
             var userEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
             var user = _appdb.Users.FirstOrDefault(u => u.Email == userEmail);
-            var transaction = _appdb.Transactions.Where(x => x.UserId == user.Id);
-            if (transaction == null)
-                return BadRequest();
+
+            int categoryId = int.Parse(id);
+            var transactions = _appdb.Transactions.Where(x => x.UserId == user.Id && x.CategoryId == categoryId).Select(
+                        t => new TransactionDTO(t.Category.Title, t.Category.Type.ToLower(), t.Amount, t.Note, t.Date.ToString("D")));
+
+            if (transactions.IsNullOrEmpty())
+                return NotFound();
             else
-            {
-                var userTransaction = transaction.Where(c => c.CategoryId == categoryId).Select(
-                        t => new TransactionDTO
-                        {
-                            //TransactionId = t.Id,
-                            CategoryName = t.Category.Title,
-                            CategoryType = t.Category.Type.ToLower(),
-                            Amount = t.Amount,
-                            Date = t.Date.ToString("D"),
-                            Note = t.Note
-                        });
-                if(userTransaction.IsNullOrEmpty()) 
-                    return NotFound();
-                else
-                    return Ok(userTransaction);
-            }
+                return Ok(transactions);
         }
 
-        [HttpGet("UserTransactionsByDate")]
+        [HttpGet("Date/{day}")]
         [Authorize]
         //https://localhost:7145/api/transactions/usertransactionsbydate?date=yyyy-MM-dd
-        public IActionResult GetTransactionsByDate(DateTime date)
+        public IActionResult GetTransactionsByDate(string day)
         {
             var userEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
             var user = _appdb.Users.FirstOrDefault(u => u.Email == userEmail);
-            var transaction = _appdb.Transactions.Where(x => x.UserId == user.Id);
-            if (transaction == null)
-                return BadRequest();
+
+            DateTime date = DateTime.Parse(day);
+            var transactions = _appdb.Transactions.Where(x => x.UserId == user.Id && x.Date == date).Select(
+                        t => new TransactionDTO(t.Category.Title, t.Category.Type.ToLower(), t.Amount, t.Note, t.Date.ToString("D")));
+
+            if (transactions.IsNullOrEmpty())
+                return NotFound();
             else
-            {
-                var userTransaction = transaction.Where(t => t.Date == date).Select(
-                        t => new TransactionDTO
-                        {
-                            //TransactionId = t.Id,
-                            CategoryName = t.Category.Title,
-                            CategoryType = t.Category.Type.ToLower(),
-                            Amount = t.Amount,
-                            Date = t.Date.ToString("D"),
-                            Note = t.Note
-                        });
-                if (userTransaction.IsNullOrEmpty())
-                    return NotFound();
-                else
-                    return Ok(userTransaction);
-            }
+                return Ok(transactions);
         }
 
-        [HttpGet("UserTransactionDetails")]
+        [HttpGet("{id}")]
         [Authorize]
         //https://localhost:7145/api/transactions/usertransactiondetails?id=0
         public IActionResult GetTransactionDetail(int id)
         {
             var userEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
             var user = _appdb.Users.FirstOrDefault(u => u.Email == userEmail);
+
             var transaction = _appdb.Transactions.FirstOrDefault(c => c.Id == id && c.UserId == user.Id);
+            
             if(transaction == null) 
+                return BadRequest();
+
+            var transactiondto = _appdb.Transactions.Where(x => x.Id == transaction.Id).Select(
+                        t => new TransactionDTO(t.Category.Title, t.Category.Type.ToLower(), t.Amount, t.Note, t.Date.ToString("D")));
+
+            if (transactiondto.IsNullOrEmpty())
                 return NotFound();
             else
-            {
-                var userTransaction = _appdb.Transactions.Where(x => x.Id == transaction.Id).Select(
-                        t => new TransactionDTO
-                        {
-                            //TransactionId = t.Id,
-                            CategoryName = t.Category.Title,
-                            CategoryType = t.Category.Type.ToLower(),
-                            Amount = t.Amount,
-                            Date = t.Date.ToString("D"),
-                            Note = t.Note
-                        });
-                return Ok(userTransaction);
-            }
+                return Ok(transactiondto);
         }
 
         [HttpPost]
@@ -217,5 +176,9 @@ namespace ExpenseTrackerApplication.Controllers
                 return BadRequest();
             }
         }
+
     }
 }
+
+
+
--- ExpenseTrackerApplication/DTO/TransactionDTO.cs
+++ ExpenseTrackerApplication/DTO/TransactionDTO.cs
@@ -8,5 +8,14 @@
         public int Amount { get; set; }
         public string Note { get; set; }
         public string Date { get; set; }
+
+        public TransactionDTO(string name, string type, int amount, string note, string date)
+        {
+            CategoryName = name;
+            CategoryType = type;
+            Amount = amount;
+            Note = note;
+            Date = date;
+        }
     }
 }
